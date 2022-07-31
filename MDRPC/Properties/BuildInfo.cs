150863a92c3fe8a67680ab1e1797f8b243c50451
Lucas Cota <lucasbrunocota@live.com>
2022-07-31 16:04:41 -0300

    Version 0.0.6.

--- MDRPC/Properties/BuildInfo.cs
+++ MDRPC/Properties/BuildInfo.cs
@@ -6,7 +6,7 @@
         public const string Description = "Enhanced Discord Rich Presence for Muse Dash.";
         public const string Author = "Brasileiro";
         public const string Company = "I love MS Lyrical Nanoha.";
-        public const string Version = "0.0.5";
+        public const string Version = "0.0.6";
         public const string DownloadLink = "https://github.com/Braasileiro/MDRPC/releases/latest";
     }
 }
